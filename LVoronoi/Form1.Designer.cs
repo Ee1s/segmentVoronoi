eb5fbeae49ed80491d96744dac5e059626dede74
Ee1s <254508313@qq.com>
2016-01-05 20:29:40 +0800

    C#

---
+++ LVoronoi/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LVoronoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "voronoi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(340, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(81, 61);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(422, 353);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ LVoronoi/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace LVoronoi
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Delaynay D_TIN = new Delaynay(); //核心功能类
+
+
+        bool bShowID = true;
+        int t = -50;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            D_TIN.DS.BBOX.XLeft = 10;
+            D_TIN.DS.BBOX.YTop = 30;
+            D_TIN.DS.BBOX.XRight = this.Width - 20;
+            D_TIN.DS.BBOX.YBottom = this.Height - D_TIN.DS.BBOX.YTop - 10;
+            DrawBoundaryBox();
+            ShowPoints(g);
+            //ShowTriangle();
+            ShowConvex(0);
+        }
+        private void DrawBoundaryBox()
+        {
+            g = this.CreateGraphics();
+            g.Clear(Color.White);
+
+            g.DrawLine(Pens.Blue, Convert.ToSingle(D_TIN.DS.BBOX.XLeft), Convert.ToSingle(D_TIN.DS.BBOX.YTop),
+                Convert.ToSingle(D_TIN.DS.BBOX.XRight), Convert.ToSingle(D_TIN.DS.BBOX.YTop));
+            g.DrawLine(Pens.Blue, Convert.ToSingle(D_TIN.DS.BBOX.XRight), Convert.ToSingle(D_TIN.DS.BBOX.YTop),
+                Convert.ToSingle(D_TIN.DS.BBOX.XRight), Convert.ToSingle(D_TIN.DS.BBOX.YBottom));
+            g.DrawLine(Pens.Blue, Convert.ToSingle(D_TIN.DS.BBOX.XRight), Convert.ToSingle(D_TIN.DS.BBOX.YBottom),
+                Convert.ToSingle(D_TIN.DS.BBOX.XLeft), Convert.ToSingle(D_TIN.DS.BBOX.YBottom));
+            g.DrawLine(Pens.Blue, Convert.ToSingle(D_TIN.DS.BBOX.XLeft), Convert.ToSingle(D_TIN.DS.BBOX.YBottom),
+                Convert.ToSingle(D_TIN.DS.BBOX.XLeft), Convert.ToSingle(D_TIN.DS.BBOX.YTop));
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //检查是否有重复点
+            for (int i = 0; i < D_TIN.DS.VerticesNum; i++)
+            {
+                if ((long)e.X == D_TIN.DS.Vertex[i].x && (long)e.Y == D_TIN.DS.Vertex[i].y)
+                    return;  //若该点已有则不再加入
+            }
+            g = this.CreateGraphics();
+            Pen mypen = new Pen(Color.Red);
+
+
+            if (D_TIN.DS.VerticesNum % 2 == 0)
+                g.DrawLine(mypen, e.X - t, e.Y + t, e.X + t, e.Y - t);
+
+            else
+                g.DrawLine(mypen, e.X + t, e.Y + t, e.X - t, e.Y - t);
+            t = t + 1;
+
+            //g.DrawLine(mypen, e.X - 50, e.Y + 50, e.X + 50, e.Y - 50);
+            //g.DrawLine(mypen, e.X + 50, e.Y + 50, e.X - 50, e.Y - 50);
+
+            //加点            
+            D_TIN.DS.Vertex[D_TIN.DS.VerticesNum].x = e.X;
+            D_TIN.DS.Vertex[D_TIN.DS.VerticesNum].y = e.Y;
+            D_TIN.DS.Vertex[D_TIN.DS.VerticesNum].ID = D_TIN.DS.VerticesNum;
+
+            if (D_TIN.DS.VerticesNum % 2 == 0)
+                g.DrawLine(mypen, e.X - t, e.Y + t, e.X + t, e.Y - t);
+
+            else
+                g.DrawLine(mypen, e.X + t, e.Y + t, e.X - t, e.Y - t);
+            t = t + 10;
+            D_TIN.DS.VerticesNum++;
+            //DrawBoundaryBox();
+            ShowPoints(g);
+            ShowTriangle();
+            //ShowConvex(50);
+            
+            
+
+        }
+        private void ShowTriangle()
+        {
+            if (D_TIN.DS.VerticesNum > 2)  //构建三角网
+                D_TIN.CreateTIN();
+
+            //输出三角形
+            for (int i = 0; i < D_TIN.DS.TriangleNum; i++)
+            {
+                Point point1 = new Point(Convert.ToInt32(D_TIN.DS.Vertex[D_TIN.DS.Triangle[i].V1Index].x), Convert.ToInt32(D_TIN.DS.Vertex[D_TIN.DS.Triangle[i].V1Index].y));
+                Point point2 = new Point(Convert.ToInt32(D_TIN.DS.Vertex[D_TIN.DS.Triangle[i].V2Index].x), Convert.ToInt32(D_TIN.DS.Vertex[D_TIN.DS.Triangle[i].V2Index].y));
+                Point point3 = new Point(Convert.ToInt32(D_TIN.DS.Vertex[D_TIN.DS.Triangle[i].V3Index].x), Convert.ToInt32(D_TIN.DS.Vertex[D_TIN.DS.Triangle[i].V3Index].y));
+
+                Pen p = new Pen(Color.Green, 1);
+                g.DrawLine(p, point1, point2);
+                g.DrawLine(p, point2, point3);
+                g.DrawLine(p, point1, point3);
+
+                if (bShowID)      //显示数字标识
+                    g.DrawString((i + 1).ToString(), new Font(FontFamily.GenericSerif, 10), Brushes.Black,
+                        (float)(point1.X + point2.X + point3.X) / 3, (float)(point1.Y + point2.Y + point3.Y) / 3);
+            }
+        }
+
+        private void ShowPoints(Graphics DC)
+        {
+            Pen p = new Pen(Color.Red, 1);
+
+            for (int i = 0; i < D_TIN.DS.VerticesNum; i++)
+            {
+                DC.DrawEllipse(p, D_TIN.DS.Vertex[i].x, D_TIN.DS.Vertex[i].y, 3, 3);
+                //if(bShowID)      //显示数字标识
+                //    g.DrawString((D_TIN.DS.Vertex[i].ID + 1).ToString(), new Font(FontFamily.GenericSerif, 8), Brushes.Blue,
+                //        (float)(D_TIN.DS.Vertex[i].x), (float)(D_TIN.DS.Vertex[i].y));
+            }
+        }
+        private void centre()
+        {
+
+            D_TIN.CalculateBC();    //求出每个三角形的圆心
+            Pen p2 = new Pen(Color.SkyBlue, 1);
+            for (int i = 0; i < D_TIN.DS.TriangleNum; i++) //显示
+            {
+                g.DrawEllipse(p2, Convert.ToSingle(D_TIN.DS.Barycenters[i].X), Convert.ToSingle(D_TIN.DS.Barycenters[i].Y), 3, 3);
+
+                //if (bShowID)      //显示数字标识
+                //    g.DrawString((i + 1).ToString(), new Font(FontFamily.GenericSerif, 9, FontStyle.Underline), Brushes.Black,
+                //        (float)(D_TIN.DS.Barycenters[i].X), (float)(D_TIN.DS.Barycenters[i].Y));
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ShowPoints(g); //显示点
+            //button2_Click(sender, e);
+            centre();
+            D_TIN.CreateVoronoi(g);
+
+        }
+        private void ShowConvex(int interval)
+        {
+            D_TIN.CreateConvex();
+
+            for (int i = 0; i < D_TIN.HullPoint.Count; i++)
+            {
+                g.DrawLine(Pens.Black, D_TIN.DS.Vertex[D_TIN.HullPoint[i]].x, D_TIN.DS.Vertex[D_TIN.HullPoint[i]].y,
+                 D_TIN.DS.Vertex[D_TIN.HullPoint[(i + 1) % D_TIN.HullPoint.Count]].x,
+                 D_TIN.DS.Vertex[D_TIN.HullPoint[(i + 1) % D_TIN.HullPoint.Count]].y);
+                Thread.Sleep(interval);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            D_TIN = new Delaynay();
+            t = -50;
+            this.Invalidate();
+        }
+
+        
+
+        
+    }
+}
